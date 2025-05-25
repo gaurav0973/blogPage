ae6ed397aad5155af156e2cb9289434f01d39fb0
gaurav0973 <codejourneypanda@gmail.com>
2025-05-26 02:54:01 +0530

    redux for state management

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,6 +1,3 @@
-import { StaticRouterProvider } from "react-router-dom"
-import { isFileLoadingAllowed } from "vite"
-
 const conf = {
     appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
