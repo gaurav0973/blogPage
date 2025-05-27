ceb87c8b7ef1fac9c1f061ec707e6880eee4b221
gaurav0973 <codejourneypanda@gmail.com>
2025-05-27 17:39:20 +0530

    login

--- src/components/index.js
+++ src/components/index.js
@@ -3,13 +3,24 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import PostCard from "./PostCard";
+
 
 export { 
     Header, 
     Footer, 
     Logo,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    PostCard
+
+
 };
 
 
